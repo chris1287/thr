b8911b4bfe55d7e72bc2cab7daf04a4db4082750
cristiano prato <cristiano.prato@gmail.com>
2018-07-05 07:34:44 +0200

    Add print cards function

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
 extern crate alsa;
 extern crate getopts;
 
+fn print_cards() {
+    let card_iterator = alsa::card::Iter::new();
+    for card in card_iterator.map(|card| card.unwrap()){
+        println!("Card#{}: {} ({})", card.get_index(), card.get_name().unwrap(), card.get_longname().unwrap());
+    }
+}
 fn main() {
 }
