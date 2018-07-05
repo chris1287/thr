744b39de5433299e7ce9deb9351a05e6d17924f7
cristiano prato <cristiano.prato@gmail.com>
2018-07-05 07:34:59 +0200

    Add print raw midis function

--- src/main.rs
+++ src/main.rs
@@ -7,5 +7,23 @@ fn print_cards() {
         println!("Card#{}: {} ({})", card.get_index(), card.get_name().unwrap(), card.get_longname().unwrap());
     }
 }
+
+fn print_rawmidis() {
+    let card_iterator = alsa::card::Iter::new();
+    for card in card_iterator.map(|card| card.unwrap()){
+        let ctl = alsa::Ctl::from_card(&card, false).unwrap();
+        let rawmidi_iterator = alsa::rawmidi::Iter::new(&ctl);
+        for rawmidi in rawmidi_iterator.map(|rawmidi| rawmidi.unwrap()) {
+            println!("rawmidi {:?} hw:{},{},{} - {} ({})",
+                rawmidi.get_stream(),
+                card.get_index(),
+                rawmidi.get_device(),
+                rawmidi.get_subdevice(),
+                card.get_name().unwrap(),
+                card.get_longname().unwrap()
+            );
+        }
+    }
+}
 fn main() {
 }
