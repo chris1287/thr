5a709a1ee560f49d896b713c396e6367c1134539
cristiano prato <cristiano.prato@gmail.com>
2018-07-09 19:19:53 +0200

    Add modulations set options

--- src/main.rs
+++ src/main.rs
@@ -88,26 +88,42 @@ fn get_amplifier(name: &str) -> u16 {
 
 fn get_knob(name: &str) -> u8 {
     match name {
-        "amplifier" => 0x00,
-        "gain" => 0x01,
-        "master" => 0x02,
-        "bass" => 0x03,
-        "middle" => 0x04,
-        "treble" => 0x05,
-        "cabinet" => 0x06,
-        "gate" => 0x5F,
-        "gate-thr" => 0x51,
-        "gate-rel" => 0x52,
-        "compressor" => 0x1F,
-        "comp-type" => 0x10,
-        "stomp-sus" => 0x11,
-        "stomp-out" => 0x12,
-        "rack-thr" => 0x11,
-        "rack-att" => 0x13,
-        "rack-rel" => 0x14,
-        "rack-ratio" => 0x15,
-        "rack-knee" => 0x16,
-        "rack-out" => 0x17,
+        "amplifier"        => 0x00,
+        "gain"             => 0x01,
+        "master"           => 0x02,
+        "bass"             => 0x03,
+        "middle"           => 0x04,
+        "treble"           => 0x05,
+        "cabinet"          => 0x06,
+        "gate"             => 0x5F,
+        "gate-thr"         => 0x51,
+        "gate-rel"         => 0x52,
+        "compressor"       => 0x1F,
+        "comp-type"        => 0x10,
+        "stomp-sus"        => 0x11,
+        "stomp-out"        => 0x12,
+        "rack-thr"         => 0x11,
+        "rack-att"         => 0x13,
+        "rack-rel"         => 0x14,
+        "rack-ratio"       => 0x15,
+        "rack-knee"        => 0x16,
+        "rack-out"         => 0x17,
+        "modulation"       => 0x2F,
+        "mod-select"       => 0x20,
+        "chorus-speed"     => 0x21,
+        "chorus-depth"     => 0x22,
+        "chorus-mix"       => 0x23,
+        "flanger-speed"    => 0x21,
+        "flanger-manual"   => 0x22,
+        "flanger-depth"    => 0x23,
+        "flanger-feedback" => 0x24,
+        "flanger-spread"   => 0x25,
+        "tremolo-freq"     => 0x21,
+        "tremolo-depth"    => 0x22,
+        "phaser-speed"     => 0x21,
+        "phaser-manual"    => 0x22,
+        "phaser-depth"     => 0x23,
+        "phaser-feedback"  => 0x24,
         _ => panic!("unrecognized knob: {}", name)
     }
 }
@@ -169,6 +185,24 @@ fn get_ratio(name: &str) -> u16 {
     }
 }
 
+fn get_modulation(name: &str) -> u16 {
+    match name {
+        "on" => 0x00,
+        "off" => 0x7F,
+        _ => panic!("unrecognized modulation: {}", name)
+    }
+}
+
+fn get_modulation_selector(name: &str) -> u16 {
+    match name {
+        "chorus" => 0x00,
+        "flanger" => 0x01,
+        "tremolo" => 0x02,
+        "phaser" => 0x03,
+        _ => panic!("unrecognized modulation selector: {}", name)
+    }
+}
+
 fn load_file(device_name: &str, file_name: &str, dry: bool) {
     let header: [u8; 18] = [
         0xF0, 0x43, 0x7D, 0x00,
@@ -221,32 +255,48 @@ fn main() {
 
     let mut opts = getopts::Options::new();
     opts.long_only(true);
-    opts.optflag("", "help", "print help");
-    opts.optflag("", "cards", "print available cards");
-    opts.optflag("", "rawmidis", "print available raw midi controllers");
-    opts.optflag("", "dryrun", "do not send sysex to device");
-    opts.optopt("", "select", "select raw midi controller", "[hw:?,?,?]");
-    opts.optopt("", "amplifier", "set amplifier", "[clean, crunch, lead, brit, modern, bass, aco, flat]");
-    opts.optopt("", "gain", "set gain", "[0-100]");
-    opts.optopt("", "master", "set master", "[0-100]");
-    opts.optopt("", "bass", "set bass", "[0-100]");
-    opts.optopt("", "middle", "set middle", "[0-100]");
-    opts.optopt("", "treble", "set treble", "[0-100]");
-    opts.optopt("", "cabinet", "set cabinet", "[usa4x12, usa2x12, brit4x12, brit2x12, cab1x12, cab4x10]");
-    opts.optopt("", "file", "load file", "[file name]");
-    opts.optopt("", "gate", "set gate", "[on, off]");
-    opts.optopt("", "gate-thr", "set gate threshold", "[0-100]");
-    opts.optopt("", "gate-rel", "set gate release", "[0-100]");
-    opts.optopt("", "compressor", "set compressor", "[on, off]");
-    opts.optopt("", "comp-type", "set compressor type", "[stomp, rack]");
-    opts.optopt("", "stomp-sus", "set compressor stomp sustain", "[0-100]");
-    opts.optopt("", "stomp-out", "set compressor stomp output", "[0-100]");
-    opts.optopt("", "rack-thr", "set compressor rack threshold", "[0-1112]");
-    opts.optopt("", "rack-att", "set compressor rack attack", "[0-100]");
-    opts.optopt("", "rack-rel", "set compressor rack release", "[0-100]");
-    opts.optopt("", "rack-ratio", "set compressor rack ratio", "[1:1, 1:4, 1:8, 1:12, 1:20, 1:inf]");
-    opts.optopt("", "rack-knee", "set compressor rack knee", "[soft, medium, hard]");
-    opts.optopt("", "rack-out", "set compressor rack output", "[0-1112]");
+    opts.optflag("" , "help"             , "print help");
+    opts.optflag("" , "cards"            , "print available cards");
+    opts.optflag("" , "rawmidis"         , "print available raw midi controllers");
+    opts.optflag("" , "dryrun"           , "do not send sysex to device");
+    opts.optopt(""  , "select"           , "select raw midi controller"             , "[hw:?, ?, ?]");
+    opts.optopt(""  , "amplifier"        , "set amplifier"                          , "[clean, crunch, lead, brit, modern, bass, aco, flat]");
+    opts.optopt(""  , "gain"             , "set gain"                               , "[0-100]");
+    opts.optopt(""  , "master"           , "set master"                             , "[0-100]");
+    opts.optopt(""  , "bass"             , "set bass"                               , "[0-100]");
+    opts.optopt(""  , "middle"           , "set middle"                             , "[0-100]");
+    opts.optopt(""  , "treble"           , "set treble"                             , "[0-100]");
+    opts.optopt(""  , "cabinet"          , "set cabinet"                            , "[usa4x12, usa2x12, brit4x12, brit2x12, cab1x12, cab4x10]");
+    opts.optopt(""  , "file"             , "load file"                              , "[file name]");
+    opts.optopt(""  , "gate"             , "set gate"                               , "[on, off]");
+    opts.optopt(""  , "gate-thr"         , "set gate threshold"                     , "[0-100]");
+    opts.optopt(""  , "gate-rel"         , "set gate release"                       , "[0-100]");
+    opts.optopt(""  , "compressor"       , "set compressor"                         , "[on, off]");
+    opts.optopt(""  , "comp-type"        , "set compressor type"                    , "[stomp, rack]");
+    opts.optopt(""  , "stomp-sus"        , "set compressor stomp sustain"           , "[0-100]");
+    opts.optopt(""  , "stomp-out"        , "set compressor stomp output"            , "[0-100]");
+    opts.optopt(""  , "rack-thr"         , "set compressor rack threshold"          , "[0-1112]");
+    opts.optopt(""  , "rack-att"         , "set compressor rack attack"             , "[0-100]");
+    opts.optopt(""  , "rack-rel"         , "set compressor rack release"            , "[0-100]");
+    opts.optopt(""  , "rack-ratio"       , "set compressor rack ratio"              , "[1:1, 1:4, 1:8, 1:12, 1:20, 1:inf]");
+    opts.optopt(""  , "rack-knee"        , "set compressor rack knee"               , "[soft, medium, hard]");
+    opts.optopt(""  , "rack-out"         , "set compressor rack output"             , "[0-1112]");
+    opts.optopt(""  , "modulation"       , "set modulation"                         , "[on, off]");
+    opts.optopt(""  , "mod-select"       , "set modulation selector"                , "[chorus, flanger, tremolo, phaser]");
+    opts.optopt(""  , "chorus-speed"     , "set chorus speed"                       , "[0-100]");
+    opts.optopt(""  , "chorus-depth"     , "set chorus depth"                       , "[0-100]");
+    opts.optopt(""  , "chorus-mix"       , "set chorus mix"                         , "[0-100]");
+    opts.optopt(""  , "flanger-speed"    , "set flanger speed"                      , "[0-100]");
+    opts.optopt(""  , "flanger-manual"   , "set flanger manual"                     , "[0-100]");
+    opts.optopt(""  , "flanger-depth"    , "set flanger depth"                      , "[0-100]");
+    opts.optopt(""  , "flanger-feedback" , "set flanger feedback"                   , "[0-100]");
+    opts.optopt(""  , "flanger-spread"   , "set flanger spread"                     , "[0-100]");
+    opts.optopt(""  , "tremolo-freq"     , "set tremolo frequency"                  , "[0-100]");
+    opts.optopt(""  , "tremolo-depth"    , "set tremolo depth"                      , "[0-100]");
+    opts.optopt(""  , "phaser-speed"     , "set phaser speed"                       , "[0-100]");
+    opts.optopt(""  , "phaser-manual"    , "set phaser manual"                      , "[0-100]");
+    opts.optopt(""  , "phaser-depth"     , "set phaser depth"                       , "[0-100]");
+    opts.optopt(""  , "phaser-feedback"  , "set phaser feedback"                    , "[0-100]");
 
     let matches = match opts.parse(&args[1..]) {
         Ok(m) => { m },
@@ -274,38 +324,38 @@ fn main() {
 
     let dry = matches.opt_present("dryrun");
 
-    let amplifier = matches.opt_str("amplifier"); 
-    match amplifier {
+    let x = matches.opt_str("amplifier"); 
+    match x {
         Some(x) => send_command(device_name.as_ref(), &get_knob("amplifier"), &get_amplifier(x.as_ref()), dry),
         None => {}
     };
 
-    let gain = matches.opt_str("gain");
-    match gain {
+    let x = matches.opt_str("gain");
+    match x {
         Some(x) => send_command(device_name.as_ref(), &get_knob("gain"), &x.parse::<u16>().unwrap(), dry),
         None => {}
     };
 
-    let master = matches.opt_str("master");
-    match master {
+    let x = matches.opt_str("master");
+    match x {
         Some(x) => send_command(device_name.as_ref(), &get_knob("master"), &x.parse::<u16>().unwrap(), dry),
         None => {}
     };
 
-    let bass = matches.opt_str("bass");
-    match bass {
+    let x = matches.opt_str("bass");
+    match x {
         Some(x) => send_command(device_name.as_ref(), &get_knob("bass"), &x.parse::<u16>().unwrap(), dry),
         None => {}
     };
 
-    let middle = matches.opt_str("middle");
-    match middle {
+    let x = matches.opt_str("middle");
+    match x {
         Some(x) => send_command(device_name.as_ref(), &get_knob("middle"), &x.parse::<u16>().unwrap(), dry),
         None => {}
     };
 
-    let treble = matches.opt_str("treble");
-    match treble {
+    let x = matches.opt_str("treble");
+    match x {
         Some(x) => send_command(device_name.as_ref(), &get_knob("treble"), &x.parse::<u16>().unwrap(), dry),
         None => {}
     };
@@ -316,20 +366,20 @@ fn main() {
         None => {}
     };
 
-    let file = matches.opt_str("file"); 
-    match file {
+    let x = matches.opt_str("file"); 
+    match x {
         Some(x) => load_file(device_name.as_ref(), &x, dry),
         None => {}
     };
 
-    let gate = matches.opt_str("gate"); 
-    match gate {
+    let x = matches.opt_str("gate"); 
+    match x {
         Some(x) => send_command(device_name.as_ref(), &get_knob("gate"), &get_gate(x.as_ref()), dry),
         None => {}
     };
 
-    let gate_thr = matches.opt_str("gate-thr");
-    match gate_thr {
+    let x = matches.opt_str("gate-thr");
+    match x {
         Some(x) => send_command(device_name.as_ref(), &get_knob("gate-thr"), &x.parse::<u16>().unwrap(), dry),
         None => {}
     };
@@ -340,63 +390,159 @@ fn main() {
         None => {}
     };
 
-    let compressor = matches.opt_str("compressor"); 
-    match compressor {
+    let x = matches.opt_str("compressor"); 
+    match x {
         Some(x) => send_command(device_name.as_ref(), &get_knob("compressor"), &get_compressor(x.as_ref()), dry),
         None => {}
     };
 
-    let compressor_type = matches.opt_str("comp-type"); 
-    match compressor_type {
+    let x = matches.opt_str("comp-type"); 
+    match x {
         Some(x) => send_command(device_name.as_ref(), &get_knob("comp-type"), &get_compressor_type(x.as_ref()), dry),
         None => {}
     };
 
-    let stomp_sus = matches.opt_str("stomp-sus");
-    match stomp_sus {
+    let x = matches.opt_str("stomp-sus");
+    match x {
         Some(x) => send_command(device_name.as_ref(), &get_knob("stomp-sus"), &x.parse::<u16>().unwrap(), dry),
         None => {}
     };
 
-    let stomp_out = matches.opt_str("stomp-out");
-    match stomp_out {
+    let x = matches.opt_str("stomp-out");
+    match x {
         Some(x) => send_command(device_name.as_ref(), &get_knob("stomp-out"), &x.parse::<u16>().unwrap(), dry),
         None => {}
     };
 
-    let rack_thr = matches.opt_str("rack-thr");
-    match rack_thr {
+    let x = matches.opt_str("rack-thr");
+    match x {
         Some(x) => send_command(device_name.as_ref(), &get_knob("rack-thr"), &x.parse::<u16>().unwrap(), dry),
         None => {}
     };
 
-    let rack_att = matches.opt_str("rack-att");
-    match rack_att {
+    let x = matches.opt_str("rack-att");
+    match x {
         Some(x) => send_command(device_name.as_ref(), &get_knob("rack-att"), &x.parse::<u16>().unwrap(), dry),
         None => {}
     };
 
-    let rack_rel = matches.opt_str("rack-rel");
-    match rack_rel {
+    let x = matches.opt_str("rack-rel");
+    match x {
         Some(x) => send_command(device_name.as_ref(), &get_knob("rack-rel"), &x.parse::<u16>().unwrap(), dry),
         None => {}
     };
 
-    let rack_ratio = matches.opt_str("rack-ratio"); 
-    match rack_ratio {
+    let x = matches.opt_str("rack-ratio"); 
+    match x {
         Some(x) => send_command(device_name.as_ref(), &get_knob("rack-ratio"), &get_ratio(x.as_ref()), dry),
         None => {}
     };
 
-    let rack_knee = matches.opt_str("rack-knee"); 
-    match rack_knee {
+    let x = matches.opt_str("rack-knee"); 
+    match x {
         Some(x) => send_command(device_name.as_ref(), &get_knob("rack-knee"), &get_knee(x.as_ref()), dry),
         None => {}
     };
 
-    let rack_out = matches.opt_str("rack-out");
-    match rack_out {
+    let x = matches.opt_str("rack-out");
+    match x {
         Some(x) => send_command(device_name.as_ref(), &get_knob("rack-out"), &x.parse::<u16>().unwrap(), dry),
+        None => {}
+    };
+
+    let x = matches.opt_str("modulation"); 
+    match x {
+        Some(x) => send_command(device_name.as_ref(), &get_knob("modulation"), &get_modulation(x.as_ref()), dry),
+        None => {}
+    };
+
+    let x = matches.opt_str("mod-select"); 
+    match x {
+        Some(x) => send_command(device_name.as_ref(), &get_knob("mod-select"), &get_modulation_selector(x.as_ref()), dry),
+        None => {}
+    };
+
+    let x = matches.opt_str("chorus-speed"); 
+    match x {
+        Some(x) => send_command(device_name.as_ref(), &get_knob("chorus-speed"), &x.parse::<u16>().unwrap(), dry),
+        None => {}
+    };
+
+    let x = matches.opt_str("chorus-depth"); 
+    match x {
+        Some(x) => send_command(device_name.as_ref(), &get_knob("chorus-depth"), &x.parse::<u16>().unwrap(), dry),
+        None => {}
+    };
+
+    let x = matches.opt_str("chorus-mix"); 
+    match x {
+        Some(x) => send_command(device_name.as_ref(), &get_knob("chorus-mix"), &x.parse::<u16>().unwrap(), dry),
+        None => {}
+    };
+
+    let x = matches.opt_str("flanger-speed"); 
+    match x {
+        Some(x) => send_command(device_name.as_ref(), &get_knob("flanger-speed"), &x.parse::<u16>().unwrap(), dry),
+        None => {}
+    };
+
+    let x = matches.opt_str("flanger-manual"); 
+    match x {
+        Some(x) => send_command(device_name.as_ref(), &get_knob("flanger-manual"), &x.parse::<u16>().unwrap(), dry),
+        None => {}
+    };
+
+    let x = matches.opt_str("flanger-depth"); 
+    match x {
+        Some(x) => send_command(device_name.as_ref(), &get_knob("flanger-depth"), &x.parse::<u16>().unwrap(), dry),
+        None => {}
+    };
+
+    let x = matches.opt_str("flanger-feedback"); 
+    match x {
+        Some(x) => send_command(device_name.as_ref(), &get_knob("flanger-feedback"), &x.parse::<u16>().unwrap(), dry),
+        None => {}
+    };
+
+    let x = matches.opt_str("flanger-spread"); 
+    match x {
+        Some(x) => send_command(device_name.as_ref(), &get_knob("flanger-spread"), &x.parse::<u16>().unwrap(), dry),
+        None => {}
+    };
+
+    let x = matches.opt_str("tremolo-freq"); 
+    match x {
+        Some(x) => send_command(device_name.as_ref(), &get_knob("tremolo-freq"), &x.parse::<u16>().unwrap(), dry),
+        None => {}
+    };
+
+    let x = matches.opt_str("tremolo-depth"); 
+    match x {
+        Some(x) => send_command(device_name.as_ref(), &get_knob("tremolo-depth"), &x.parse::<u16>().unwrap(), dry),
+        None => {}
+    };
+
+    let x = matches.opt_str("phaser-speed"); 
+    match x {
+        Some(x) => send_command(device_name.as_ref(), &get_knob("phaser-speed"), &x.parse::<u16>().unwrap(), dry),
+        None => {}
+    };
+
+    let x = matches.opt_str("phaser-manual"); 
+    match x {
+        Some(x) => send_command(device_name.as_ref(), &get_knob("phaser-manual"), &x.parse::<u16>().unwrap(), dry),
+        None => {}
+    };
+
+    let x = matches.opt_str("phaser-depth"); 
+    match x {
+        Some(x) => send_command(device_name.as_ref(), &get_knob("phaser-depth"), &x.parse::<u16>().unwrap(), dry),
+        None => {}
+    };
+
+    let x = matches.opt_str("phaser-feedback"); 
+    match x {
+        Some(x) => send_command(device_name.as_ref(), &get_knob("phaser-feedback"), &x.parse::<u16>().unwrap(), dry),
         None => {}
     };
 
